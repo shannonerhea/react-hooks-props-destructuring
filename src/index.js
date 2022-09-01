868886d52029a0b7abdca401a224ce3dadb2cfe7
shannone <shannone426@gmail.com>
2022-09-01 09:42:45 -0400

    complete

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+// i have zero idea wth is going on with this lab! are there instructions or am i just supposed to copy the code thats in canvas. ughh 
